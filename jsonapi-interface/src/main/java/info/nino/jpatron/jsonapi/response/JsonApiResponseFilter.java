6f6127218440262a74e63c04d9156358c52f20a0
nino888 <nino.cerovec@hotmail.com>
2023-09-21 21:54:01 +0200

    JsonApiResponseFilter - implemented Content-Type header injection for @JsonApi endpoints

--- jsonapi-interface/src/main/java/info/nino/jpatron/jsonapi/response/JsonApiResponseFilter.java
+++ jsonapi-interface/src/main/java/info/nino/jpatron/jsonapi/response/JsonApiResponseFilter.java
@@ -1,16 +1,15 @@
 package info.nino.jpatron.jsonapi.response;
 
 import info.nino.jpatron.annotiation.JsonApi;
-import javax.inject.Inject;
+import info.nino.jpatron.jsonapi.JsonApiMediaType;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
-
-import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * JSON:API response filter implementation
@@ -19,23 +18,18 @@ import java.util.logging.Logger;
 @Provider
 public class JsonApiResponseFilter implements ContainerResponseFilter
 {
-	@Inject
-	private Logger log;
-
-	//@Context
-	//private HttpServletResponse request;	//maven: javax.servlet.servlet-api:2.5
-
 	@Context
 	ResourceInfo ri;
 
 	@Override
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-		if (responseContext.getEntity() instanceof JsonApiResponse) {//!ri.getResourceClass().isAnnotationPresent(JsonApi.class)) {
-			//TODO
-			//responseContext.getHeaders().add("Content-Type", "application/vnd.api+json");
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+	{
+		if((ri.getResourceClass().isAnnotationPresent(JsonApi.class) || ri.getResourceMethod().isAnnotationPresent(JsonApi.class))
+			&& responseContext.getEntity() instanceof JsonApiResponse)
+		{
+			responseContext.setEntity(responseContext.getEntity(),
+				responseContext.getEntityAnnotations(),
+				MediaType.valueOf(JsonApiMediaType.APPLICATION_JSON_API));
 		}
-
-		//responseContext.setEntity(
-		//		 new JsonApiResponse(responseContext.getEntity()), null, MediaType.APPLICATION_JSON_TYPE);
 	}
 }
